ea59970364657de2610c1f21b6f64dad79633411
linto <lintov7@gmail.com>
2022-12-06 18:18:58 -0500

    Changed UI

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask,current_app
-from flask_pymongo import PyMongo
+from flask import Flask,current_app, jsonify, request, abort
+from flask_pymongo import PyMongo, ObjectId
 from flask_apscheduler import APScheduler
 import pandas as pd
 import urllib
 from bson import json_util
 import json
+import numpy as np
 
-app = Flask(__name__, static_url_path='', static_folder='static',)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 
 app.config["MONGO_URI"] = "mongodb+srv://linto_mongodb:BtAUQm3cbhms28WO@cluster0.x3jziue.mongodb.net/flask_db?retryWrites=true&w=majority"
@@ -47,27 +48,36 @@ scheduler.start()
 def indexPage():
     return current_app.send_static_file('index.htm')
 
+@app.route("/api/listings/")
+def getAll():
+    skipString = request.args.get('skip', '')
+    skip = 0
+    try: 
+        skip = int(skipString)
+    except:
+        print("Error converting to int")
+    allLisiting = listings.find({}).limit(10).skip(skip)
+    return json.loads(json_util.dumps(allLisiting))
+
+@app.route("/api/listings/<path:id>")
+def getItemById(id):
+    try:
+        item = listings.find_one({"_id" : ObjectId(id)})
+        if item == None:
+            return {"error": "Item not found"}
+        return json.loads(json_util.dumps(item))
+    except:
+         return {"error": "Please enter valid objectID"}
+
 @app.route("/api/listings/count")
 def listingCount():
     return {"count": listings.count_documents({})}
-    # user = listings.find_one({},{"_id": 0})
-    # print(user)
-    # return json.loads(json_util.dumps(user))
-    # return jsonify(json_util.dumps(user))
-    # return {"count": listings.count_documents()}
 
 @app.route("/api/listings/license")
 def listingLicense():
-    a =listings.aggregate([
-      {
-      "$group" :
-        {
-          "_id" : "$room_type",
-          "count": { "$sum": 1 }
-        }  
-      }
-    ])
-    return json.loads(json_util.dumps(a))
+    hasLicense =listings.count_documents({"license":{"$ne":np.nan}})
+    noLicense =listings.count_documents({"license":np.nan})
+    return {"hasLicense": hasLicense, "noLicense": noLicense}
 
 @app.route("/api/listings/roomtype")
 def listingRoomType():
@@ -88,7 +98,25 @@ def listingRoomType():
     ])
     return json.loads(json_util.dumps(roomTypesCount))
 
-@app.route("/api/listings/neighbourhood_price")
+@app.route("/api/listings/neighbourhood")
 def listingNeighbourhoodPrice():
-    return "<p>Hello, Worfld!</p>"
+    neighbourhoodCount =listings.aggregate([
+      {
+      "$group" :
+        {
+          "_id" : "$neighbourhood",
+          "count": { "$sum": 1 }
+        },
+      },
+       { "$sort" : { "count" : -1 } },
+        { "$limit" : 10},
+      {"$project": { 
+            "_id": 0,
+            "name": "$_id",
+            "count": 1,
+            "sum": 1
+        }},
+        
+    ])
+    return json.loads(json_util.dumps(neighbourhoodCount))
 
